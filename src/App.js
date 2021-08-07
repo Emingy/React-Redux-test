45844a17df5c22564e6112e493cc2026ed989bc0
Alexander Potapov <potapov.a.i@iptp.net>
2021-08-07 20:21:44 +0300

    Init and add table of products

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import Table from "./components/Table";
 
+
+var def = [
+  {
+  id: 1,
+  name: "test name",
+  price: 12000,
+  amount: 2,
+  sum: 24000
+  },
+  {
+    id: 2,
+    name: "test name",
+    price: 2000,
+    amount: 2,
+    sum: 4000
+  },
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container pt-3">
+      <Table/>
+      {/* <table className="table table-bordered table-responsive border-primary">
+        <thead>
+          <tr>
+            <th colspan="5" rowspan="1">
+              <h1 className="text-center">Название раздела</h1>
+            </th>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Название товара</th>
+            <th>Цена</th>
+            <th>Количество</th>
+            <th>Сумма</th>
+          </tr>
+        </thead>
+        <tbody>
+        
+        </tbody>
+      </table> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { compose, createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
+import { rootReducer } from './redusers/rootReducer';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(
+      thunk
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+)
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
